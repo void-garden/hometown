4ed273300273eef524c0cc4d1f3b1c0f5c933698
Darius Kazemi <darius.kazemi@gmail.com>
2018-09-05 08:12:18 -0700

    Add music player to mp3 links
    This commit adds a check to all links in statuses in the web client. If
    it detects an mp3 file, it inserts an mp3 player after the link. This
    will be wrapped up in CW tags with everything else.

--- app/javascript/mastodon/components/status_content.js
+++ app/javascript/mastodon/components/status_content.js
@@ -58,6 +58,8 @@ export default class StatusContent extends React.PureComponent {
         }
       } else if (link.textContent[0] === '#' || (link.previousSibling && link.previousSibling.textContent && link.previousSibling.textContent[link.previousSibling.textContent.length - 1] === '#')) {
         link.addEventListener('click', this.onHashtagClick.bind(this, link.text), false);
+      } else if (link.href && link.href.match(/\.mp3$/)) {
+        link.innerHTML += `<br><br><audio class="fc-audio" controls src="${link.href}"></audio>`;
       } else {
         link.setAttribute('title', link.href);
       }
