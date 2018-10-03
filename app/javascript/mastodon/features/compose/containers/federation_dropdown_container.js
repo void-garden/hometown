9059cd9204e64707cc7b21987a19c9ab49942946
Darius Kazemi <darius.kazemi@gmail.com>
2018-10-02 22:21:45 -0700

    Bugfixes and localization for default federation settings

--- app/javascript/mastodon/features/compose/containers/federation_dropdown_container.js
+++ app/javascript/mastodon/features/compose/containers/federation_dropdown_container.js
@@ -6,7 +6,7 @@ import { isUserTouching } from '../../../is_mobile';
 
 const mapStateToProps = state => ({
   isModalOpen: state.get('modal').modalType === 'ACTIONS',
-  value: state.getIn(['compose', 'federation']),
+  value: (state.getIn(['compose', 'federation']) ? true : false),
 });
 
 const mapDispatchToProps = dispatch => ({
